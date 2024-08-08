13d16c9e8b6097bbdd633f1594f28a472904880d
Geoffrey PECHIN <geoffrey.pechin@gmail.com>
2024-08-08 10:35:24 +0200

    harmonia_5 add new table in shema.sql

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,19 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+CREATE TABLE category (
+  id INT UNSIGNED PRIMARY KEY auto_increment NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE program (
+  id INT UNSIGNED PRIMARY KEY auto_increment NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  synopsis TEXT NOT NULL,
+  poster VARCHAR(255) NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  year INT ,
+  category_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY(category_id) REFERENCES category(id)
+);
