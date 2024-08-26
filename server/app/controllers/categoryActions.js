2524dc7fefc802242fd1ca7f894328c6fd751581
Geoffrey PECHIN <geoffrey.pechin@gmail.com>
2024-08-26 11:29:01 +0200

    harmonia 6 done

--- server/app/controllers/categoryActions.js
+++ server/app/controllers/categoryActions.js
@@ -1,3 +1,4 @@
+const tables = require("../../database/tables");
 // Some data to make the trick
 
 const categories = [
@@ -13,16 +14,12 @@ const categories = [
   
   // Declare the actions
 
-  const browse = (req, res) => {
-    if (req.query.q != null) {
-        const filteredCategories = categories.filter((categorie) =>
-            categorie.name.includes(req.query.q)
-    );
-    res.json(filteredCategories);
-  } else {
-    res.json(categories);
-  }
-};
+  const browse = async (req, res) => {
+      const categoriesFromDB = await tables.category.readAll();
+    
+      res.json(categoriesFromDB);
+    };
+
 
 const read = (req, res) => {
     const parseId = parseInt(req.params.id, 10);
